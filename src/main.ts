a3b1954dc8eb39124dd3adb529d6fed0ab1752f3
DESKTOP-BGE4RRB\User <hachimansuvivel@gmail.com>
2026-04-18 15:19:01 +0700

    feat/security: add disable-devtool on production

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import DisableDevtool from 'disable-devtool'
 import './assets/index.css'
+
+// Chỉ kích hoạt chống Inspect / Console trên môi trường Production
+if (import.meta.env.PROD) {
+  DisableDevtool({
+    clearLog: true,     // Tự động xóa Console
+    disableMenu: true,  // Chặn menu chuột phải
+  })
+}
 
 const app = createApp(App)
 const pinia = createPinia()
